a629b269e992f802069837fbca8a3c47796742e7
Bruno Rodrigues <47644815+Bruno977@users.noreply.github.com>
2021-02-25 16:54:01 -0300

    Adiciona Upper e Lower Case

--- scriptClientUbuntu.sh
+++ scriptClientUbuntu.sh
@@ -8,6 +8,8 @@ serverDomainUpCase=TIITANET.LOCAL
 serverNameAndDomain=servidor.tiitanet.local
 serverNameAndDomainUpCase=SERVIDOR.TIITANET.LOCAL
 
+
+
 #modifica o arquivo /etc/hosts com os dados do servidor
 #sed -i "/127.0.1.1/a $serverIp	$serverName $serverNameAndDomain" /etc/hosts
 
@@ -19,17 +21,37 @@ serverNameAndDomainUpCase=SERVIDOR.TIITANET.LOCAL
 #touch /etc/krb5.conf
 
 
-#escreve no arquivo /etc/krb5.conf
-cat > '/etc/krb5.conf' <<EOT
-[libdefaults]
-default_realm = $serverDomainUpCase
-rdns = no
-dns_lookup_kdc = true
-dns_lookup_realm = true
 
-[realms]
-$serverDomainUpCase = {
-kdc = $serverNameAndDomain
-admin_server = $serverNameAndDomain
-}
-EOT
+
+#escreve no arquivo /etc/krb5.conf
+#cat > '/etc/krb5.conf' <<EOT
+#[libdefaults]
+#default_realm = $serverDomainUpCase
+#rdns = no
+#dns_lookup_kdc = true
+#dns_lookup_realm = true
+
+#[realms]
+#$serverDomainUpCase = {
+#kdc = $serverNameAndDomain
+#admin_server = $serverNameAndDomain
+#}
+#EOT
+
+
+
+
+#colocar senha do administrator
+#kinit administrator
+#klist
+#sleep 3
+
+
+echo 'Informe o nome do PC: '
+read namePc
+
+namePcUpperCase=$(echo $namePc | tr [a-z] [A-Z])
+namePcLowerCase=$(echo $namePc | tr [A-Z] [a-z])
+
+echo $namePcLowerCase
+
