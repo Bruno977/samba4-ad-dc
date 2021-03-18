57a3d7f783b9edfec8b92bda1d57b974b078946b
Bruno Rodrigues <47644815+Bruno977@users.noreply.github.com>
2021-03-18 13:53:23 -0300

    Add dhclient

--- scriptClientUbuntuNfs.sh
+++ scriptClientUbuntuNfs.sh
@@ -15,11 +15,16 @@ cat > '/etc/rc.local'<<EOT
 #!bin/bash
 EOT
 
-sed -i "/bash/a showmount -e $serverIp" /etc/rc.local
+sed -i "/bash/a dhclient" /etc/rc.local
+
+sed -i "/dhclient/a showmount -e $serverIp" /etc/rc.local
 
 sed -i "/showmount/a  mount -t nfs $serverIp:$nameServer $nameClient" /etc/rc.local
 
 sed -i "/nfs/a mount | grep nfs" /etc/rc.local
 
-
 sed -i "/errors=remount/a $serverIp:$nameServer     $nameClient    nfs     defaults        0       0" /etc/fstab
+
+sed -i "/grep/a exit 0" /etc/rc.local
+
+sudo chmod 755 /etc/rc.local
